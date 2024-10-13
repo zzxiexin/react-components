7e90c84e4cc684cafad68b75140c036c52a01d91
yasinyuan <yasinyuan@yasinyuandeMacBook-Air.local>
2024-10-13 22:43:57 +0800

    feat: 修改组件目录结构

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/react-components/',
+  publicPath: '/react-components/',
   outputPath: 'docs-dist',
   themeConfig: {
-    name: 'react-components',
+    name: 'yxhooks',
+  },
+  resolve: {
+    atomDirs: [
+      { type: 'component', dir: './src/components' },
+      { type: 'hook', dir: './src/hooks' },
+      { type: 'util', dir: './src/utils' },
+    ],
   },
 });
