974fd6efccb4fa86765d74621c633788829e48b6
zzxiexin <zzxiexin@users.noreply.github.com>
2024-10-16 17:08:01 +0000

    deploy: 6648645f30b76e0899af0546be43e8238832d9ec

---
+++ meta__components.919d67c0.async.js
@@ -0,0 +1,120 @@
+(self.webpackChunkyxx_component=self.webpackChunkyxx_component||[]).push([[838],{76936:function(l,e,n){"use strict";var a;n.r(e),n.d(e,{demos:function(){return L}});var v=n(90228),r=n.n(v),p=n(87999),d=n.n(p),o=n(75271),O=n(98297),h=n(8904),L={"button-demo-default":{component:o.memo(o.lazy(function(){return n.e(570).then(n.bind(n,62390))})),asset:{type:"BLOCK",id:"button-demo-default",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(4047).Z},react:{type:"NPM",value:"18.3.1"},"yxx-component":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"yxx-component":h},renderOpts:{compile:function(){var y=d()(r()().mark(function i(){var s,x=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(19).then(n.bind(n,54019));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,x));case 3:case"end":return t.stop()}},i)}));function f(){return y.apply(this,arguments)}return f}()}},"button-demo-size":{component:o.memo(o.lazy(function(){return n.e(570).then(n.bind(n,28979))})),asset:{type:"BLOCK",id:"button-demo-size",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(6618).Z},react:{type:"NPM",value:"18.3.1"},"yxx-component":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"yxx-component":h},renderOpts:{compile:function(){var y=d()(r()().mark(function i(){var s,x=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(19).then(n.bind(n,54019));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,x));case 3:case"end":return t.stop()}},i)}));function f(){return y.apply(this,arguments)}return f}()}},"button-demo-disabled":{component:o.memo(o.lazy(function(){return n.e(570).then(n.bind(n,96086))})),asset:{type:"BLOCK",id:"button-demo-disabled",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(86476).Z},react:{type:"NPM",value:"18.3.1"},"yxx-component":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"yxx-component":h},renderOpts:{compile:function(){var y=d()(r()().mark(function i(){var s,x=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(19).then(n.bind(n,54019));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,x));case 3:case"end":return t.stop()}},i)}));function f(){return y.apply(this,arguments)}return f}()}},"button-demo-canclick":{component:o.memo(o.lazy(function(){return n.e(570).then(n.bind(n,75559))})),asset:{type:"BLOCK",id:"button-demo-canclick",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(84276).Z},react:{type:"NPM",value:"18.3.1"},"yxx-component":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"yxx-component":h},renderOpts:{compile:function(){var y=d()(r()().mark(function i(){var s,x=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(19).then(n.bind(n,54019));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,x));case 3:case"end":return t.stop()}},i)}));function f(){return y.apply(this,arguments)}return f}()}},"button-demo-throttle":{component:o.memo(o.lazy(function(){return n.e(570).then(n.bind(n,45461))})),asset:{type:"BLOCK",id:"button-demo-throttle",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(25647).Z},react:{type:"NPM",value:"18.3.1"},"yxx-component":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"yxx-component":h},renderOpts:{compile:function(){var y=d()(r()().mark(function i(){var s,x=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(19).then(n.bind(n,54019));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,x));case 3:case"end":return t.stop()}},i)}));function f(){return y.apply(this,arguments)}return f}()}},"button-demo-ghost":{component:o.memo(o.lazy(function(){return n.e(570).then(n.bind(n,44968))})),asset:{type:"BLOCK",id:"button-demo-ghost",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(56408).Z},react:{type:"NPM",value:"18.3.1"},"yxx-component":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"yxx-component":h},renderOpts:{compile:function(){var y=d()(r()().mark(function i(){var s,x=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(19).then(n.bind(n,54019));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,x));case 3:case"end":return t.stop()}},i)}));function f(){return y.apply(this,arguments)}return f}()}},"button-demo-circle":{component:o.memo(o.lazy(function(){return n.e(570).then(n.bind(n,85123))})),asset:{type:"BLOCK",id:"button-demo-circle",refAtomIds:["Button"],dependencies:{"index.tsx":{type:"FILE",value:n(28612).Z},react:{type:"NPM",value:"18.3.1"},"yxx-component":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{react:a||(a=n.t(o,2)),"yxx-component":h},renderOpts:{compile:function(){var y=d()(r()().mark(function i(){var s,x=arguments;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(19).then(n.bind(n,54019));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,x));case 3:case"end":return t.stop()}},i)}));function f(){return y.apply(this,arguments)}return f}()}}}},66107:function(l,e,n){"use strict";n.r(e),n.d(e,{demos:function(){return r}});var a=n(75271),v=n(63962),r={}},8904:function(l,e,n){"use strict";n.r(e),n.d(e,{Button:function(){return S}});var a=n(26068),v=n.n(a),r=n(82092),p=n.n(r),d=n(48305),o=n.n(d),O=n(67825),h=n.n(O),L=n(75271),y="yxx-",f=function(c){return"".concat(y).concat(c)};function i(u){var c,B,m="";if(typeof u=="string"||typeof u=="number")m+=u;else if(typeof u=="object")if(Array.isArray(u)){var A=u.length;for(c=0;c<A;c++)u[c]&&(B=i(u[c]))&&(m&&(m+=" "),m+=B)}else for(B in u)u[B]&&(m&&(m+=" "),m+=B);return m}function s(){for(var u,c,B=0,m="",A=arguments.length;B<A;B++)(u=arguments[B])&&(c=i(u))&&(m&&(m+=" "),m+=c);return m}var x=s,P=n(52676),t=["type","size","ghost","shape","children","disabled","icon","debounce","throttle","onClick","throttleDom"],D=f("btn");function S(u){var c=u.type,B=u.size,m=u.ghost,A=m===void 0?!1:m,K=u.shape,U=u.children,F=u.disabled,G=u.icon,T=u.debounce,I=u.throttle,g=u.onClick,z=u.throttleDom,H=h()(u,t),J=(0,L.useState)(F),W=o()(J,2),R=W[0],b=W[1],E=(0,L.useRef)(),Q=(0,L.useState)(0),N=o()(Q,2),j=N[0],$=N[1],V=function(M){E!=null&&E.current&&clearTimeout(E==null?void 0:E.current),E.current=setTimeout(function(){R||g&&(g==null||g(M))},T?typeof T=="boolean"?1e3:T*1e3:0)},X=function(M){var C=I?typeof I=="boolean"?1:I:0;$(C),b(!0),E.current=setInterval(function(){C>0&&(R||b(!0),C=C-1,$(C)),C<=0&&(g&&(g==null||g(M)),b(!1),clearInterval(E.current))},1e3)};return(0,P.jsxs)("button",v()(v()({},H),{},{className:x(D,p()(p()(p()(p()(p()({},"".concat(D,"-").concat(c),c),"".concat(D,"-").concat(B),B),"".concat(D,"-").concat(c,"-ghost"),A),"".concat(D,"-").concat(c,"-").concat(K),K),"".concat(D,"-").concat(c,"-disabled"),R)),onClick:function(M){T&&V(M),I&&!R&&X(M),!I&&!T&&g&&(g==null||g(M))},children:[G,I&&j?(0,P.jsxs)("div",{children:[j,z||"s\u540E\u53EF\u70B9\u51FB"]}):null,I&&j?null:U]}))}},82250:function(l,e,n){"use strict";n.r(e),n.d(e,{texts:function(){return v}});var a=n(98297);const v=[{value:"\u6309\u94AE\u6709\u516D\u79CD\u7C7B\u578B\uFF1A\u9ED8\u8BA4\u6309\u94AE\u3001\u4E3B\u9898\u6309\u94AE\u3001\u865A\u7EBF\u6309\u94AE\u3001\u5371\u9669\u6309\u94AE\u3001\u94FE\u63A5\u6309\u94AE\u548C\u6587\u672C\u6309\u94AE\u3002",paraId:0,tocIndex:0},{value:"\u6309\u94AE\u6709\u5927\u3001\u4E2D\u3001\u5C0F\u4E09\u79CD\u5C3A\u5BF8\u3002",paraId:1,tocIndex:1},{value:"\u901A\u8FC7\u8BBE\u7F6E size \u4E3A large small \u5206\u522B\u628A\u6309\u94AE\u8BBE\u4E3A\u5927\u3001\u5C0F\u5C3A\u5BF8\u3002\u82E5\u4E0D\u8BBE\u7F6E size\uFF0C\u5219\u5C3A\u5BF8\u4E3A\u4E2D\u3002",paraId:2,tocIndex:1},{value:`\u6DFB\u52A0 disabled \u5C5E\u6027\u5373\u53EF\u8BA9\u6309\u94AE\u5904\u4E8E\u4E0D\u53EF\u7528\u72B6\u6001\uFF0C\u6309\u94AE\u4E8B\u4EF6\u5C06\u4E0D\u4F1A\u88AB\u6267\u884C\uFF0C\u540C\u65F6\u6309\u94AE\u6837\u5F0F\u4E5F\u4F1A\u6539\u53D8\u3002
+`,paraId:3,tocIndex:2},{value:`\u914D\u7F6E\u5012\u8BA1\u65F6\u6309\u94AE\uFF0C\u5728\u914D\u7F6E\u7684\u65F6\u95F4\u8FC7\u540E\u624D\u53EF\u70B9\u51FB\u3002
+`,paraId:4,tocIndex:3},{value:`\u914D\u7F6E\u5012\u8BA1\u65F6\u6309\u94AE\uFF0C\u5728\u914D\u7F6E\u7684\u65F6\u95F4\u8FC7\u540E\u624D\u53EF\u70B9\u51FB\u3002
+`,paraId:5,tocIndex:4},{value:`\u5E7D\u7075\u6309\u94AE\u5C06\u6309\u94AE\u7684\u5185\u5BB9\u53CD\u8272\uFF0C\u80CC\u666F\u53D8\u4E3A\u900F\u660E\uFF0C\u5E38\u7528\u5728\u6709\u8272\u80CC\u666F\u4E0A
+`,paraId:6,tocIndex:5},{value:`\u901A\u8FC7\u6DFB\u52A0shape\uFF1A circle\uFF0C\u53EF\u5C06\u6309\u94AE\u8BBE\u7F6E\u6210\u5706\u5F62\uFF0C\u4F46\u662F\u5982\u679C\u6587\u5B57\u957F\u5BBD\u4E0D\u76F8\u7B49\uFF0C\u5219\u51FA\u4E0D\u4E86\u5706\u5F62\u6548\u679C
+`,paraId:7,tocIndex:6}]},43346:function(l,e,n){"use strict";n.r(e),n.d(e,{texts:function(){return v}});var a=n(63962);const v=[]},84276:function(l,e){"use strict";e.Z=`import React from 'react';
+import { Button } from 'yxx-component';
+
+export default function App() {
+  return (
+    <div id="component-demo">
+      <Button type="primary" debounce={3} onClick={() => console.log('click')}>
+        \u9632\u6296\u6309\u94AE
+      </Button>
+    </div>
+  );
+}
+`},28612:function(l,e){"use strict";e.Z=`import React from 'react';
+import { Button } from 'yxx-component';
+
+export default function App() {
+  return (
+    <div id="component-demo">
+      <Button type="primary" shape="circle" />
+      <Button type="dashed" shape="circle" />
+      <Button type="danger" shape="circle" />
+    </div>
+  );
+}
+`},4047:function(l,e){"use strict";e.Z=`import React from 'react';
+import { Button } from 'yxx-component';
+
+export default function App() {
+  return (
+    <div id="component-demo">
+      <Button>Default</Button>
+      <Button type="primary" onClick={() => console.log('click')}>
+        Primary
+      </Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="danger">Danger</Button>
+      <Button type="link">Link</Button>
+      <Button type="text">Text</Button>
+    </div>
+  );
+}
+`},86476:function(l,e){"use strict";e.Z=`import React from 'react';
+import { Button } from 'yxx-component';
+
+export default function App() {
+  return (
+    <div id="component-demo">
+      <Button type="primary" disabled onClick={() => console.log('click')}>
+        Primary disabled
+      </Button>
+      <Button type="dashed" size="small" disabled>
+        Dashed disabled
+      </Button>
+    </div>
+  );
+}
+`},56408:function(l,e){"use strict";e.Z=`import React from 'react';
+import { Button } from 'yxx-component';
+
+export default function App() {
+  return (
+    <div
+      id="component-demo"
+      style={{ background: 'rgb(190, 200, 200)', padding: 10 }}
+    >
+      <Button ghost onClick={() => console.log('click')}>
+        Default
+      </Button>
+      <Button type="primary" ghost>
+        Primary
+      </Button>
+      <Button type="dashed" ghost>
+        Dashed
+      </Button>
+      <Button type="danger" ghost>
+        Danger
+      </Button>
+      <Button type="link" ghost>
+        Link
+      </Button>
+      <Button type="text" ghost>
+        Text
+      </Button>
+    </div>
+  );
+}
+`},6618:function(l,e){"use strict";e.Z=`import React from 'react';
+import { Button } from 'yxx-component';
+
+export default function App() {
+  return (
+    <div id="component-demo">
+      <Button type="primary" size="large">
+        Large
+      </Button>
+      <Button type="danger">Default</Button>
+      <Button type="dashed" size="small">
+        Small
+      </Button>
+    </div>
+  );
+}
+`},25647:function(l,e){"use strict";e.Z=`import React from 'react';
+import { Button } from 'yxx-component';
+
+export default function App() {
+    return (
+        <div id="component-demo">
+            <Button type="primary" throttle={10} onClick={() => console.log('click')} throttleDom={<div style={{ display: 'inline' }}>\u4F60\u5E72\u561B\uFF5E\uFF5E</div>}>
+                \u8282\u6D41\u6309\u94AE
+            </Button>
+        </div>
+    );
+}
+`},67825:function(l,e,n){var a=n(64382);function v(r,p){if(r==null)return{};var d=a(r,p),o,O;if(Object.getOwnPropertySymbols){var h=Object.getOwnPropertySymbols(r);for(O=0;O<h.length;O++)o=h[O],!(p.indexOf(o)>=0)&&Object.prototype.propertyIsEnumerable.call(r,o)&&(d[o]=r[o])}return d}l.exports=v,l.exports.__esModule=!0,l.exports.default=l.exports},64382:function(l){function e(n,a){if(n==null)return{};var v={},r=Object.keys(n),p,d;for(d=0;d<r.length;d++)p=r[d],!(a.indexOf(p)>=0)&&(v[p]=n[p]);return v}l.exports=e,l.exports.__esModule=!0,l.exports.default=l.exports}}]);
